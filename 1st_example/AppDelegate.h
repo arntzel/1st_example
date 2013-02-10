e66a2a60976910b722fdcd4080ab1c1aaaa4ac6a
Eliot <arntzel@gmail.com>
2013-02-10 15:11:18 -0500

    Initial Commit

---
+++ 1st_example/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  1st_example
+//
+//  Created by Eliot Arntz on 2/10/13.
+//  Copyright (c) 2013 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ 1st_example/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  1st_example
+//
+//  Created by Eliot Arntz on 2/10/13.
+//  Copyright (c) 2013 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
